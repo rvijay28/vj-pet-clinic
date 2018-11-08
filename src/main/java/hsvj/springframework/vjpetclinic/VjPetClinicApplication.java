5fe81c2f26c68c464db1ca66c3df9f7632731b28
vraghave <vraghave@VRAGHAVE7JXH3H2.opentext.net>
2018-11-08 09:08:42 +0530

    My first commit

---
+++ src/main/java/hsvj/springframework/vjpetclinic/VjPetClinicApplication.java
@@ -0,0 +1,12 @@
+package hsvj.springframework.vjpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VjPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/hsvj/springframework/vjpetclinic/VjPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package hsvj.springframework.vjpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VjPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
